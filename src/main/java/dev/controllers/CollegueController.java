80d2663e35378370c294e055d6123bdc40f09c5e
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-18 12:10:59 +0200

    Ajout d'une réponse à l'utilisateur lorsqu'un collègue est correctement affiché

--- src/main/java/dev/controllers/CollegueController.java
+++ src/main/java/dev/controllers/CollegueController.java
@@ -5,6 +5,8 @@ package dev.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,8 +41,9 @@ public class CollegueController
 	}
 
 	@PostMapping
-	public void afficherCollegueParMatricule(@RequestBody Collegue collegueAAjouter)
+	public ResponseEntity<String> afficherCollegueParMatricule(@RequestBody Collegue collegueAAjouter)
 	{
 		collegueService.ajouterUnCollegue(collegueAAjouter);
+		return ResponseEntity.status(HttpStatus.OK).body("Collègue ajouté correctement");
 	}
 }
