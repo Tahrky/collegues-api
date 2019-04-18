24d83321ffd8e75423212bf556764e3eb363eca4
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-18 14:15:44 +0200

    Ajout de tests unitaires pour les fonctions modifierPhotoUrl et modifierMail

--- src/test/java/dev/services/CollegueServiceTest.java
+++ src/test/java/dev/services/CollegueServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import dev.entities.Collegue;
 import dev.exception.CollegueInvalideException;
+import dev.exception.CollegueNonTrouveException;
 
 /**
  *
@@ -18,12 +19,16 @@ import dev.exception.CollegueInvalideException;
 public class CollegueServiceTest
 {
 	CollegueService collegueService = new CollegueService();
+
 	static String nomCorrect = "Denis";
 	static String prenomCorrect = "Bob";
 	static String emailCorrect = "email@a.com";
 	static String photoUrlCorrect = "http://photoUrl.com";
+
 	final static String TIME_PATTERN = "yyyy-MM-d";
 	static LocalDate dateDeNaissanceCorrect = LocalDate.parse("1982-12-11", DateTimeFormatter.ofPattern(TIME_PATTERN));
+
+	static String matricule = "1";
 
 	@Test(expected = CollegueInvalideException.class)
 	public void ajouterUnCollegue_NomTropCourt()
@@ -65,5 +70,35 @@ public class CollegueServiceTest
 	{
 		collegueService.ajouterUnCollegue(
 				new Collegue(nomCorrect, prenomCorrect, emailCorrect, photoUrlCorrect, LocalDate.now()));
+	}
+
+	@Test(expected = CollegueInvalideException.class)
+	public void modifierEmail_EmailTropCourt()
+	{
+		collegueService.modifierEmail(matricule, "aa");
+	}
+
+	@Test(expected = CollegueInvalideException.class)
+	public void modifierEmail_EmailSansArobase()
+	{
+		collegueService.modifierEmail(matricule, "arthurLeChevalier.com");
+	}
+
+	@Test(expected = CollegueNonTrouveException.class)
+	public void modifierEmail_mauvaisMatricule()
+	{
+		collegueService.modifierEmail("2", emailCorrect);
+	}
+
+	@Test(expected = CollegueInvalideException.class)
+	public void modifierPhotoUrl_photoUrlMauvaise()
+	{
+		collegueService.modifierPhotoUrl(matricule, "maPhoto.com");
+	}
+
+	@Test(expected = CollegueNonTrouveException.class)
+	public void modifierPhotoUrl_mauvaisMatricule()
+	{
+		collegueService.modifierPhotoUrl("2", photoUrlCorrect);
 	}
 }
