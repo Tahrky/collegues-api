67c0b7880d5409f4c553af868079bf684b7e67d7
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-04-19 11:59:20 +0200

    Création d'une classe d'initialisation de la BDD

--- src/main/java/dev/repository/CollegueRepository.java
+++ src/main/java/dev/repository/CollegueRepository.java
@@ -3,10 +3,6 @@
  */
 package dev.repository;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.entities.Collegue;
@@ -17,22 +13,5 @@ import dev.entities.Collegue;
  */
 public interface CollegueRepository extends JpaRepository<Collegue, String>
 {
-	final String TIME_PATTERN = "yyyy-MM-d";
 
-	default void init()
-	{
-		LocalDate date1 = LocalDate.parse("1950-11-14", DateTimeFormatter.ofPattern(TIME_PATTERN));
-
-		String matriculeTemp = UUID.randomUUID().toString();
-		Collegue colTemp = new Collegue(matriculeTemp, "Bob", "Robert", "a@a.a", "http://photoQuiExistePas.com", date1);
-		this.save(colTemp);
-
-		matriculeTemp = UUID.randomUUID().toString();
-		this.save(new Collegue(matriculeTemp, "Adeline", "Noelle", "b@b.b", "http://photoQuiE*xistePas.com", date1));
-
-		matriculeTemp = UUID.randomUUID().toString();
-		this.save(new Collegue(matriculeTemp, "Bob", "Arthur", "c@c.c", "http://p*hotoQuiExistePas.com", date1));
-
-		this.save(new Collegue("1", "Bobbi", "Albert", "a@a.a", "http://photoQuiExi*stePas.com", date1));
-	}
 }
---
+++ src/main/java/dev/repository/StartupDataInit.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package dev.repository;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import dev.entities.Collegue;
+
+/**
+ *
+ * @author BIRABEN-BIANCHI Hugo
+ */
+@Component
+public class StartupDataInit
+{
+	@Autowired
+	CollegueRepository collegueRepo;
+	final String TIME_PATTERN = "yyyy-MM-d";
+
+	// La méthode init va être invoquée au démarrage de l'application.
+	@EventListener(ContextRefreshedEvent.class)
+	public void init()
+	{
+		LocalDate date1 = LocalDate.parse("1950-11-14", DateTimeFormatter.ofPattern(TIME_PATTERN));
+
+		String matriculeTemp = UUID.randomUUID().toString();
+		collegueRepo.save(new Collegue(matriculeTemp, "Bob", "Robert", "a@a.a", "http://photoQuiExistePas.com", date1));
+
+		matriculeTemp = UUID.randomUUID().toString();
+		collegueRepo.save(
+				new Collegue(matriculeTemp, "Adeline", "Noelle", "b@b.b", "http://photoQuiE*xistePas.com", date1));
+
+		matriculeTemp = UUID.randomUUID().toString();
+		collegueRepo
+				.save(new Collegue(matriculeTemp, "Bob", "Arthur", "c@c.c", "http://p*hotoQuiExistePas.com", date1));
+
+		collegueRepo.save(new Collegue("1", "Bobbi", "Albert", "a@a.a", "http://photoQuiExi*stePas.com", date1));
+	}
+}
